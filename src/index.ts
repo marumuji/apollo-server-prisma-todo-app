075a6e9204a70ba70020e1c311ad0331518f8e33
marumuji <marumuji@gmail.com>
2022-06-20 22:44:39 +0900

    Externalization of schema file

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { ApolloServer } from 'apollo-server';
+import { join } from 'path'
 
-const typeDefs =  gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
+  loaders: [new GraphQLFileLoader()],
+});
 
 const books = [
   {
@@ -32,7 +25,8 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
+const server = new ApolloServer({ schema: schemaWithResolvers });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
